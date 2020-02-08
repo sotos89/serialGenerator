1be79533168a98089e5ebd396cb3222fb80a4879
giannis <janis_s89@hotmail.com>
2020-02-08 10:37:18 +0200

    Initial Commit

---
+++ serial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  serial
+//
+//  Created by giannis  on 08/02/2020.
+//  Copyright © 2020 giannis . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
